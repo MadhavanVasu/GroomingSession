7fdcdc8013ec5f9b20c8442c9469c4ef063b1af9
madv <madhavan.v@publicissapient.com>
2023-03-23 13:05:55 +0530

    Inheritance, Interface and Ordinal in Enum

---
+++ Week1/src/main/java/com/mv/week1/part1/questions/EnumExtendAndInterface.java
@@ -0,0 +1,43 @@
+package com.mv.week1.part1.questions;
+
+interface MyInterface {
+
+    public void setStatus(String orderStatus);
+
+}
+
+// Enum can implement interface
+enum OrderStatus implements MyInterface {
+    DISPATCHED,
+    SHIPPED,
+    OUT_FOR_DELIVERY,
+    DELIVERED;
+
+    String orderStatus;
+
+    @Override
+    public void setStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
+
+// Cannot extend from enum. Causes compilation error
+//class A extends OrderStatus{
+//
+//}
+
+
+public class EnumExtendAndInterface {
+
+    public static void main(String[] args) {
+        OrderStatus delivered = OrderStatus.DELIVERED;
+        delivered.setStatus("Package Delivered");
+        System.out.println(delivered);
+        System.out.println(delivered.orderStatus);
+
+        // ordinal() method returns the position of the enum constant
+        for (OrderStatus status : OrderStatus.values())
+            System.out.println(status + " " + status.ordinal());
+    }
+
+}
--- Week1/src/main/java/com/mv/week1/part1/questions/ForAndForEach.java
+++ Week1/src/main/java/com/mv/week1/part1/questions/ForAndForEach.java
@@ -1,4 +1,18 @@
 package com.mv.week1.part1.questions;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ForAndForEach {
+
+    public static void main(String[] args) {
+
+        List<Integer> arrayList = new ArrayList<>(List.of(1, 2,3,4,5,6));
+        List<Integer> linkedList = new LinkedList<>(List.of(7,8,9,10,1));
+
+
+
+    }
+
 }
