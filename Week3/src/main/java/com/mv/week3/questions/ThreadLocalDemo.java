72f8b72f58ce2ceacfe2670fb0e1a0016af436fe
madv <madhavan.v@publicissapient.com>
2023-04-11 16:51:44 +0530

    Added comments for ThreadLocal

--- Week3/src/main/java/com/mv/week3/questions/ThreadLocalDemo.java
+++ Week3/src/main/java/com/mv/week3/questions/ThreadLocalDemo.java
@@ -4,7 +4,18 @@ package com.mv.week3.questions;
 public class ThreadLocalDemo {
     static class Task implements Runnable {
 
+        // Thread local provides a way to create thread-local variables,
+        // which are variables that are local to each thread and are not shared among threads.
+        // Each thread has its own copy of the thread-local variable,
+        // which is stored in a separate memory space specific to that thread.
+        // It has methods like get(), set(), remove().
+        // The get() method retrieves the value of the thread-local variable for the calling thread
+        // The set() method sets the value of the thread-local variable for the calling thread
+        // The remove() method removes the value of the thread-local variable for the calling thread.
         private ThreadLocal<Integer> localVar = new ThreadLocal<Integer>() {
+            // Anonymous inner class
+            // Here we are overriding the ThreadLocal class' initial value method as it returns null.
+            // Here instead we are returning 0.
             @Override
             protected Integer initialValue() {
                 return 0;
