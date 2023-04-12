ac26f8978c52e5dde4db2f7a0dd5bb0804dbf7b5
madv <madhavan.v@publicissapient.com>
2023-04-12 12:59:04 +0530

    Made changes in ClassLevelLock file

--- Week3/src/main/java/com/mv/week3/questions/ClassLevelLock.java
+++ Week3/src/main/java/com/mv/week3/questions/ClassLevelLock.java
@@ -2,51 +2,37 @@ package com.mv.week3.questions;
 
 class Student implements Runnable {
 
-    static Integer count = 1;
+    static int count = 1;
+    static Object obj = new Object();
 
-//    public void display() throws InterruptedException {
-//        // Every class in Java has a unique lock which is nothing but a class level lock.
-//        // If a thread wants to execute a static synchronized method, then the thread requires a class level lock.
-//        // Once a thread got the class level lock, then it is allowed to execute any static synchronized method of that class.
-//        // Once method execution completes automatically thread releases the lock.
-//
-////        synchronized (count)
-//        synchronized (Student.class) {
-//            System.out.println("Inside synchronized block " + Thread.currentThread().getName());
-//            Thread.sleep(1000);
-//            count++;
-//        }
-//
-//    }
-
-    public synchronized void display() throws InterruptedException {
+    public void display() throws InterruptedException {
         // Every class in Java has a unique lock which is nothing but a class level lock.
         // If a thread wants to execute a static synchronized method, then the thread requires a class level lock.
         // Once a thread got the class level lock, then it is allowed to execute any static synchronized method of that class.
         // Once method execution completes automatically thread releases the lock.
-
-//        synchronized (count)
-//        synchronized (Student.class) {
+        // Static variables in Java are shared by all instances of a class
+        // and are stored in the class's memory space, not in the local cache of individual threads.
+        // unprotected modification of shared state isn't guaranteed to be visible in other threads
+        // without setting up appropriate memory barriers.
+        // It's entirely fine for those changes to be visible to other threads immediately - it's just not guaranteed.
+        // A race condition occurs when two or more threads can access shared data,
+        // and they try to change it at the same time.
+        // We use synchronisation to avoid race condition.
+        // Can acquire class level lock using static object as monitor or using class name.
+        // synchronized (obj) or synchronized(Classname.class)
+        synchronized (Student.class) {
             System.out.println("Inside synchronized block " + Thread.currentThread().getName());
-        Thread.sleep(3000);
+            Thread.sleep(1000);
+            count++;
+            System.out.println("Count incremented in display()");
+        }
 
-        count++;
-
-//        }
-
-    }
-
-    public synchronized void objectMethod() {
-        System.out.println("Inside non static method");
-        count++;
-        System.out.println("Count incremented");
     }
 
     @Override
     public void run() {
         try {
             display();
-            objectMethod();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
