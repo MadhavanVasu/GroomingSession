21f3b4edda7b1e88bd4427b31a369a1637332d61
madv <madhavan.v@publicissapient.com>
2023-04-18 17:56:40 +0530

    Added Multithreading in Singleton class

---
+++ Week5/src/main/java/com/mv/week5/MultithreadingSingletonClass.java
@@ -0,0 +1,59 @@
+package com.mv.week5;
+
+public class MultithreadingSingletonClass {
+
+    // Have to use volatile keyword here. Otherwise, double-checking dangerous
+    // Because it can lead to racing condition.
+    private static volatile MultithreadingSingletonClass obj = null;
+    private String name;
+
+    private MultithreadingSingletonClass() {
+
+    }
+
+    private MultithreadingSingletonClass(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public static synchronized MultithreadingSingletonClass getInstanceSynchronizedMethod() {
+        if (obj == null)
+            obj = new MultithreadingSingletonClass();
+        return obj;
+    }
+
+    public static MultithreadingSingletonClass getInstanceSynchronizedBlock() {
+        // Here we do double-checking
+        if (obj == null) {
+            synchronized (MultithreadingSingletonClass.class) {
+                if (obj == null)
+                    obj = new MultithreadingSingletonClass(Thread.currentThread().getName());
+            }
+        }
+        return obj;
+    }
+
+}
+
+
+class MultithreadingSingletonDemo {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            MultithreadingSingletonClass obj = MultithreadingSingletonClass.getInstanceSynchronizedBlock();
+            System.out.println(obj.getName());
+        });
+        Thread t2 = new Thread(() -> {
+            MultithreadingSingletonClass obj = MultithreadingSingletonClass.getInstanceSynchronizedBlock();
+            System.out.println(obj.getName());
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+}
