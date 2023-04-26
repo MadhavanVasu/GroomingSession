a211f5de4a39a3a614691bd7de8253c790cbe755
madv <madhavan.v@publicissapient.com>
2023-04-26 18:38:43 +0530

    Added Flyweight design pattern

--- Week5/src/main/java/com/mv/week5/behavioraldesignpatterns/flyweight/Tree.java
+++ Week5/src/main/java/com/mv/week5/behavioraldesignpatterns/flyweight/Tree.java
@@ -6,6 +6,8 @@ public class Tree {
     // This constant data of an object is usually called the intrinsic state.
     // It lives within the object; other objects can only read it, not change it.
     // Here it is name and color of the tree
+    // If we need to create a new Tree Object everytime so that we can store it in some Collections,
+    // we can move the intrinsic field to a different class (TreeType) and factory should return TreeType.
     private final String name;
     private final Color color;
 
