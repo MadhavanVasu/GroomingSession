16393c339faea9a4e506dd3fe182ff21296bd626
madv <madhavan.v@publicissapient.com>
2023-04-27 16:05:06 +0530

    Added comments in Command Design Pattern

--- Week5/src/main/java/com/mv/week5/behavioraldesignpatterns/command/MainClass.java
+++ Week5/src/main/java/com/mv/week5/behavioraldesignpatterns/command/MainClass.java
@@ -2,6 +2,14 @@ package com.mv.week5.behavioraldesignpatterns.command;
 
 public class MainClass {
 
+    // Command is a behavioral design pattern that turns a request into a stand-alone object
+    // that contains all information about the request.
+
+    // It achieves :
+    // Single Responsibility Principle - You can decouple classes that invoke operations
+    // from classes that perform these operations.
+    // Open/Closed Principle - You can introduce new commands into the app without breaking
+    // existing client code.
     public static void main(String[] args) {
         Television television = new Television();
         Command command = new OnCommand(television);
